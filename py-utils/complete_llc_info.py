3ecfb8fb4998ffcdf8038a2f9bc3dc13455f7d36
amreo <amreo@users.noreply.github.com>
2019-05-05 20:36:57 +0200

    Improved complete_llc_info.py

--- py-utils/complete_llc_info.py
+++ py-utils/complete_llc_info.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 import sys, json
-import l40p
 import tempfile
 import subprocess
 import time
 
 data = json.load(sys.stdin)
 for item in data:
-    print (">>>", item["id"], file=sys.stderr)
+    print ("ID:", item["id"], file=sys.stderr)
     with tempfile.NamedTemporaryFile() as temp: 
-        print(temp.name, file=sys.stderr)
+        print("    TMP:", temp.name, file=sys.stderr)
         temp.write(bytes.fromhex(item["data"]))
         temp.flush()
-        ksdump = subprocess.run(["ksdump", "--format", "json", temp.name, "ksy/L40P.ksy"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-        item["parsed_data"] = json.loads(ksdump.stdout.decode())
+        
+        ksdump = subprocess.run(["ksdump", "--format", "json", temp.name, "ksy/L40P.ksy"], capture_output=True)
+        if ksdump.returncode == 0:
+            item["parsed_data"] = json.loads(ksdump.stdout.decode())
+        else:
+            json.dump(item, sys.stderr)
+            print("    STDERR:", ksdump.stderr.decode(), file=sys.stderr)
+            item["parsed_data"] = "ERROR!"
+        
 json.dump(data, sys.stdout)
